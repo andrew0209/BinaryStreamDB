db9cafbbc7a3c88444dac5327cf5e43d1e7e430b
Maksym Borukh <maksboruh@gmail.com>
2019-04-16 23:51:14 +0300

    add owner model

---
+++ Stream/models/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stream.models
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Number { get; set; }
+
+        public Car(string b, string m, int n)
+        {
+            Brand = b;
+            Model = m;
+            Number = n;
+        }
+    }
+}
---
+++ Stream/models/Owner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stream.models
+{
+    public class Owner
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Car> Cars { get; set; }
+
+        public Owner()
+        {
+            FirstName = "";
+            LastName = "";
+            Cars = null;
+        }
+        public Owner(string fn, string ln)
+        {
+            FirstName = fn;
+            LastName = ln;
+            Cars = null;
+        }
+
+        public Owner(string fn, string ln, List<Car> cars)
+        {
+            FirstName = fn;
+            LastName = ln;
+            Cars = new List<Car>(cars);
+        }
+    }
+}
