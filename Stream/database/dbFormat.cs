b73c7d46d3fd9a566293bada4f93c6f59492e33a
Maksym Borukh <maksboruh@gmail.com>
2019-04-17 03:03:37 +0300

    add db format

---
+++ Stream/database/dbFormat.cs
@@ -0,0 +1,9 @@
+﻿namespace Stream.database
+{
+    public class dbFormat
+    {
+        int AmountOfTable { get; set; }
+        string Name { get; set; }
+        int AmountOfItems { get; set; }
+    }
+}
--- Stream/operations/OwnerCRD.cs
+++ Stream/operations/OwnerCRD.cs
@@ -11,7 +11,47 @@ namespace Stream.operations
         public string path = Path.Combine(Environment.CurrentDirectory, "DataBase.dat");
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            string newPath = Path.Combine(Environment.CurrentDirectory, "NewDataBase.dat");
+            try
+            {
+                FileStream fs = new FileStream("NewDataBase.dat", FileMode.CreateNew);
+                fs.Close();
+                fs.Dispose();
+                using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
+                {
+                    reader.Read();
+                    while (reader.PeekChar() != -1)
+                    {
+                        var car = new Owner();
+                        car.Id = reader.ReadInt32();
+                        car.Brand = reader.ReadString();
+                        car.Model = reader.ReadString();
+                        car.Number = reader.ReadInt32();
+                        car.OwnerId = reader.ReadInt32();
+                        if (car.Id != id)
+                        {
+                            Insert(car, newPath);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            try
+            {
+                File.Delete(Path.Combine(Environment.CurrentDirectory, "backup.dat"));
+                File.Move("DataBase.dat", "backup.dat");
+                File.Delete(path);
+                File.Move("NewDataBase.dat", "DataBase.dat");
+                File.Delete(newPath);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         public List<Owner> GetAll()
